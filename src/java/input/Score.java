fa31071805f9b98661a20cb397ca29c6f5c303e8
barbara <web038projekt@gmail.com>
2012-05-21 19:38:18 +0200

    teil von david integriert

---
+++ src/java/input/Score.java
@@ -0,0 +1,45 @@
+package at.ac.big.tuwien.ewa.twitter;
+
+
+/**
+ * Used to publish a score on twitter
+ * @author pl
+ *
+ */
+public class Score {
+	
+	private String uuid;
+	private String playerName;
+	
+	
+	public Score (String uuid, String playerName) {
+		this.uuid = uuid;
+		this.playerName = playerName;
+	}
+	
+	public Score() {
+		
+	}
+	
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getTwitterPublicationString() {
+		return this.playerName + " highscore result UUID " + uuid;		
+	}
+
+	
+	
+}
