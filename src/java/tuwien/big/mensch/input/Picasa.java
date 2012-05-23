851bc9353769e8b0bacc25c4f3d5c9aeceb6b9b1
barbara <web038projekt@gmail.com>
2012-05-23 20:49:40 +0200

    versuch picasa avatare in register einzufuegen

--- src/java/tuwien/big/mensch/input/Picasa.java
+++ src/java/tuwien/big/mensch/input/Picasa.java
@@ -10,6 +10,7 @@ import com.google.gdata.data.*;
 import com.google.gdata.data.media.*;
 import com.google.gdata.data.photos.*;
 import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
 
 /*
  * To change this template, choose Tools | Templates
@@ -21,13 +22,29 @@ import java.util.ArrayList;
  *
  * @author Barbara
  */
+@ManagedBean(name = "pic")
 public class Picasa implements PicasaConnector{
     
     ArrayList<Avatar> avatars = new ArrayList<Avatar>();
 
+    public ArrayList<Avatar> getAvatars() {
+        return avatars;
+    }
+
+    public void setAvatars(ArrayList<Avatar> avatars) {
+        this.avatars = avatars;
+    }
+
+    public Picasa() {
+    }
+
+    
+    
     @Override
     public List<Avatar> getPhotoURLs() throws Exception {
+        System.out.println("get avatars"); 
         
+        try{
         URL feedUrl = new URL("https://picasaweb.google.com/111420671758947023853/EWA2012"); 
 
         PicasawebService myService = new PicasawebService("Picasa");
@@ -40,9 +57,11 @@ public class Picasa implements PicasaConnector{
             a1.setUrl(photo.getMediaThumbnails().get(0).getUrl());
             avatars.add(a1);
         }
+        }catch(Exception x){
+        System.out.println(x.getStackTrace()); 
+        }
         
         return avatars; 
-    }
-    
+    }   
 }
 
